9230f3b5dfb092123688e32d937adc43288645d8
Ben Blackman <bblkmn5@gmail.com>
2017-12-11 23:16:52 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,9 +44,6 @@ class UsersController < ApplicationController
   get '/logout' do
     if logged_in?
       session[:user_id].clear
-      redirect '/'
-    else
       redirect '/login'
-    end
   end
 end
