c9da838632e58d125926c8bc51cbbc317cf54dd1
Ben Blackman <bblkmn5@gmail.com>
2017-12-11 22:19:40 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,27 @@
 class UsersController < ApplicationController
 
-get '/signup' do
+  get '/signup' do
 
-  erb :'/users/create_user'
-end
-
-post '/signup' do
-  if params[:username] == "" || params[:email] == "" || params[:password] == ""
-    redirect to '/signup'
-  else
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    @user.save
-    session[:id] = @user.id
-    redirect "/tweets"
+    erb :'/users/create_user'
   end
-end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user.save
+      session[:id] = @user.id
+      redirect "/tweets"
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'users/signup'
+    else
+      erb :'/tweets/tweets'
+    end
+  end
 
 end
