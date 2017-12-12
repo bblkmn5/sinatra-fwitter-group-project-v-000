e59d94db17db2f27bd0321a7b4d5c156696c664c
Ben Blackman <bblkmn5@gmail.com>
2017-12-12 00:20:30 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -51,7 +51,7 @@ class TweetsController < ApplicationController
 
   patch '/tweets/:id' do
     if params[:content] == ""
-      redirect '/tweets/:id/edit'
+      redirect '/tweets/edit_tweet'
     else
       @tweet = Tweet.find_by_id(params[:id])
       @tweet.content = params[:content]
